80ad4770b73ae8e3fc47de139346cb0f8aeedf3f
bikkina <rajvvbikkina@gmail.com>
2020-04-11 22:11:42 +0530

    <1137> ascii vlues

---
+++ src/javaproj/Variablesdata.java
@@ -0,0 +1,22 @@
+package javaproj;
+
+public class Variablesdata {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		char c ='A';
+		char Q = 'B';
+		char Y = ' ';
+		char t = ' ';
+		
+		
+		System.out.println((float)c);
+		System.out.println((int)Q);
+		System.out.println((char)267);
+		System.out.println((double)t);
+		
+	}
+
+}
